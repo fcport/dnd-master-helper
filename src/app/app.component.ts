10d94d054fd531e75f3826acd272bfc6e5bdd926
fcport <federico.casadei.portfolio@gmail.com>
2024-09-25 19:35:31 +0200

    feat: upload file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,12 @@ import {Component, signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {
   ChatCompletionChunk,
-  ChatCompletionMessage,
   ChatCompletionRequestBase,
   CreateMLCEngine,
   MLCEngine
 } from "@mlc-ai/web-llm";
 import {ChatCompletion} from "@mlc-ai/web-llm/lib/openai_api_protocols/chat_completion";
-import * as repl from "node:repl";
-
+import * as pdfjsLib from 'pdfjs-dist';
 
 @Component({
   selector: 'app-root',
@@ -24,6 +22,7 @@ export class AppComponent {
   engine?: MLCEngine
   progress = signal(0)
   loadingMessage = signal("Loading model...")
+  pdfContent = signal("");
 
 
   async initEngine() {
@@ -42,29 +41,73 @@ export class AppComponent {
   }
 
   async testEngine() {
-    if(this.engine) {
-      const messages: ChatCompletionRequestBase = {
-        messages: [
-          {role: "assistant", content: "You are a helpful AI assistant."},
-          {role: "user", content: "What is the capital of France?"},
-        ],
-        stream: false
-      }
 
+    if (!this.engine) return;
 
-      const reply: ChatCompletion | AsyncIterable<ChatCompletionChunk> = await this.engine.chat.completions.create(
-        messages
-      );
-      if('choices' in reply) {
-        console.log(reply.choices[0].message.content);
-
-      }else{
-        console.log("Streamed response:", reply);
-      }
+    const messages: ChatCompletionRequestBase = {
+      messages: [
+        {role: "assistant", content: "You are a helpful AI assistant."},
+        {role: "user", content: "What is the capital of France?"},
+      ],
+      stream: false
     }
+
+
+    const reply: ChatCompletion | AsyncIterable<ChatCompletionChunk> = await this.engine.chat.completions.create(
+      messages
+    );
+    if ('choices' in reply) {
+      console.log(reply.choices[0].message.content);
+
+    } else {
+      console.log("Streamed response:", reply);
+    }
+
   }
 
 
+  async onFileSelected(event: any) {
+    if (!event.target) return;
+    const file = event.target.files[0];
+    const arrayBuffer = await file.arrayBuffer();
+    const pdf = await pdfjsLib.getDocument(arrayBuffer).promise;
+
+    let fullText = '';
+    for (let i = 1; i <= pdf.numPages; i++) {
+      const page = await pdf.getPage(i);
+      const textContent = await page.getTextContent();
+      fullText += textContent.items.map((item: any) => item.str).join(' ');
+    }
+
+    this.pdfContent.set(fullText) ;
+
+
+    this.askQuestion()
+  }
+
+
+  async askQuestion() {
+    if (!this.engine) return;
+
+    const messages: ChatCompletionRequestBase = {
+      messages: [
+        {role: "assistant", content: `You are a helpful AI assistant that can answer questions about docs, this is the doc content: ${this.pdfContent()}`},
+        {role: "user", content: "What is the content of this document? Can you summarize it?"},
+      ],
+      stream: false
+    }
+
+
+    const reply: ChatCompletion | AsyncIterable<ChatCompletionChunk> = await this.engine.chat.completions.create(
+      messages
+    );
+    if ('choices' in reply) {
+      console.log(reply.choices[0].message.content);
+
+    } else {
+      console.log("Streamed response:", reply);
+    }
+  }
 }
 
 
