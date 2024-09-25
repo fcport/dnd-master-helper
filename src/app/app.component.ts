81e5bae8b04a69ce1b2497ef1be4394876c11d55
fcport <federico.casadei.portfolio@gmail.com>
2024-09-25 16:43:38 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CreateMLCEngine, MLCEngine} from "@mlc-ai/web-llm";
+
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,29 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'dnd-master-helper';
+
+  engine?: MLCEngine
+
+
+  async initEngine() {
+
+
+    const initProgressCallback = (initProgress: any) => {
+      console.log(initProgress);
+    }
+    const selectedModel = "Llama-3.1-8B-Instruct-q4f32_1-MLC";
+
+   this.engine = await CreateMLCEngine(
+      selectedModel,
+      { initProgressCallback: (initProgress)=> {
+          console.log(initProgress);
+
+        } }, // engineConfig
+    );
+  }
+
+
 }
+
+
+// Callback function to update model loading progress
