2d280e43b9f148e397048c6ad8c89754e220487d
fcport <federico.casadei.portfolio@gmail.com>
2024-09-29 17:59:01 +0200

    fix: articles

--- src/app/components/document-loader/document-loader.component.ts
+++ src/app/components/document-loader/document-loader.component.ts
@@ -27,16 +27,18 @@ export class DocumentLoaderComponent implements OnInit {
 
   articles = this.documentsService.documents
 
-  sortedDocumentsAphabetically = computed(() => this.articles().sort(
-    (a: Doc, b: Doc) => {
-      if (a.title! < b.title!) {
-        return -1;
-      }
-      if (a.title! > b.title!) {
-        return 1;
-      }
-      return 1
-    }))
+  sortedDocumentsAphabetically = computed(() =>
+    this.articles.length === 0 ? [] :
+      this.articles().sort(
+        (a: Doc, b: Doc) => {
+          if (a.title! < b.title!) {
+            return -1;
+          }
+          if (a.title! > b.title!) {
+            return 1;
+          }
+          return 1
+        }))
 
   loadingDocumentsMessage = injectAppSelector(selectLoadingDocumentsMessage)
   isLoadingDocuments = injectAppSelector(selectTotalLoadingDocuments)
