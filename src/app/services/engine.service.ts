e0ab2c3178622c6eef30385f61a841e1c0500635
fcport <federico.casadei.portfolio@gmail.com>
2024-10-01 22:41:39 +0200

    fix: model

--- src/app/services/engine.service.ts
+++ src/app/services/engine.service.ts
@@ -49,7 +49,7 @@ export class EngineService {
   async initEngine() {
     this.loadingEngine.set(true);
     // const selectedModel = "Llama-3.1-8B-Instruct-q4f32_1-MLC";
-    const selectedModel = "mlc-chat-stanford-crfm-music-small-800k-q0f32-MLC";
+    const selectedModel = "Phi-3-mini-4k-instruct-q4f32_1-MLC";
     try {
       const engine = await CreateMLCEngine(
         selectedModel,
