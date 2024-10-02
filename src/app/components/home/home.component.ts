3a3501a2a9568006d34a500e05c8ed2fe83a74b5
fcport <federico.casadei.portfolio@gmail.com>
2024-10-02 17:32:45 +0200

    feat: home page content

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,18 +1,19 @@
 import {Component, inject} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 import {EngineService} from "../../services/engine.service";
+import {RouterLink, RouterModule} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-    imports: [
-        NgOptimizedImage
-    ],
+  imports: [
+    NgOptimizedImage,
+    RouterModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
-  engineService = inject(EngineService);
 
 }
