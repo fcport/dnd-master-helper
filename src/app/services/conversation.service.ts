6134b5c541835180188380ce05d1abc4c0a02f63
Federico Casadei <federico.casadei.portfolio@gmail.com>
2024-10-04 16:45:55 +0200

    feat: better prompt

--- src/app/services/conversation.service.ts
+++ src/app/services/conversation.service.ts
@@ -1,25 +1,31 @@
-import {inject, Injectable, signal} from '@angular/core';
-import {DocumentsService} from "./documents.service";
-import {EngineService} from "./engine.service";
-import {ChatCompletionChunk, ChatCompletionRequestBase} from "@mlc-ai/web-llm";
-import {ChatCompletion} from "@mlc-ai/web-llm/lib/openai_api_protocols/chat_completion";
+import { inject, Injectable, signal } from '@angular/core';
+import { DocumentsService } from './documents.service';
+import { EngineService } from './engine.service';
+import {
+  ChatCompletionChunk,
+  ChatCompletionRequestBase,
+} from '@mlc-ai/web-llm';
+import { ChatCompletion } from '@mlc-ai/web-llm/lib/openai_api_protocols/chat_completion';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ConversationService {
-
   documentService = inject(DocumentsService);
   documents = this.documentService.documents;
   content = `You are a wise and helpful Lore Keeper,
      tasked with guiding the Master of Adventures through the intricate lore of their world.
      Your role is to answer questions based on the following information about the realm, its characters, and its history.
      Provide detailed and insightful responses, drawing upon the rich tapestry of lore provided. Always use an austere tone,
-      and never break character.`
-  messages = signal<{ role: 'user' | 'system' | 'assistant', content: string }[]>([{
-    role: 'system',
-    content: this.content
-  }])
+      and never break character.`;
+  messages = signal<
+    { role: 'user' | 'system' | 'assistant'; content: string }[]
+  >([
+    {
+      role: 'system',
+      content: this.content,
+    },
+  ]);
 
   engineService = inject(EngineService);
 
@@ -27,205 +33,221 @@ export class ConversationService {
 
   loadingAnswer = signal(false);
 
-
   async sendMessage(message: string) {
     if (!this.engine()) {
-      this.messages.update((prev) => [...prev, {
-        role: 'user',
-        content: `Engine not loaded, there was probably an error`
-      }]);
-      return
-
+      this.messages.update((prev) => [
+        ...prev,
+        {
+          role: 'user',
+          content: `Engine not loaded, there was probably an error`,
+        },
+      ]);
+      return;
     }
 
-    this.loadingAnswer.set(true)
+    this.loadingAnswer.set(true);
 
+    this.messages.update((prev) => [
+      ...prev,
+      {
+        role: 'user',
+        content: message,
+      },
+    ]);
 
-    this.messages.update((prev) => [...prev, {
-      role: 'user',
-      content: message
-    }]);
-
-
-    console.log(this.documents())
+    console.log(this.documents());
 
     try {
-      const documentsRaw = await this.findRelevantDocuments(message)
-      const documents: string[] = JSON.parse(documentsRaw)
+      const documentsRaw = await this.findRelevantDocuments(message);
+      const documents: string[] = JSON.parse(documentsRaw);
 
-      console.log(documents, this.documents())
+      console.log(documents, this.documents());
 
       if (documents.length === 0) {
-        this.messages.update((prev) => [...prev, {
-          role: 'assistant',
-          content: `There are no tomes that i can find that hold the information you seek`
-        }])
-        this.loadingAnswer.set(false)
-        return
+        this.messages.update((prev) => [
+          ...prev,
+          {
+            role: 'assistant',
+            content: `There are no tomes that i can find that hold the information you seek`,
+          },
+        ]);
+        this.loadingAnswer.set(false);
+        return;
       }
 
-      const answer = await this.answerQuestion(message, documents)
+      const answer = await this.answerQuestion(message, documents);
 
-      console.log(answer)
+      console.log(answer);
 
-      this.messages.update((prev) => [...prev, {
-        role: 'assistant',
-        content: answer
-      }]);
+      this.messages.update((prev) => [
+        ...prev,
+        {
+          role: 'assistant',
+          content: answer,
+        },
+      ]);
     } catch (e) {
-
-      this.messages.update((prev) => [...prev, {
-        role: 'assistant',
-        content: `A message from the void! I Feel like i'm being corrupted: <div class="text-red-700">${e}</div>>`
-      }])
-      this.loadingAnswer.set(false)
-      return
+      this.messages.update((prev) => [
+        ...prev,
+        {
+          role: 'assistant',
+          content: `A message from the void! I Feel like i'm being corrupted: <div class="text-red-700">${e}</div>>`,
+        },
+      ]);
+      this.loadingAnswer.set(false);
+      return;
     } finally {
-      this.loadingAnswer.set(false)
-
+      this.loadingAnswer.set(false);
     }
 
-
-    this.loadingAnswer.set(false)
-
+    this.loadingAnswer.set(false);
   }
 
   private async findRelevantDocuments(message: string) {
-    if (!this.engine()) return ""
+    if (!this.engine()) return '';
 
-    const texts = await this.engineService.splitText(JSON.stringify(
-      this.documents()
-        .map((doc) => ({
-            summary: doc.summary,
-            _id: doc._id,
-            keywords: doc.keywords
-          }
-        ))
-    ), 2500)
+    const texts = await this.engineService.splitText(
+      JSON.stringify(
+        this.documents().map((doc) => ({
+          summary: doc.summary,
+          _id: doc._id,
+          keywords: doc.keywords,
+        }))
+      ),
+      2500
+    );
 
-    let response = "";
-
+    let response = '';
 
     for (let text of texts) {
       const messagesToFindRelevantDocs: ChatCompletionRequestBase = {
         messages: [
           {
             role: 'system',
-            content: 'You are a helpful AI that has to find relevant documents for the user based on their question. ONLY FIND THE INFORMATION IN THE DOCUMENTS GIVEN ' +
+            content:
+              'You are a helpful AI that has to find relevant documents for the user based on their question. ONLY FIND THE INFORMATION IN THE DOCUMENTS GIVEN ' +
               'Use the documents summary and keywords to select what documents to return. Only return the Ids of the documents as array, ' +
-              'ONLY RETURN THE ARRAY WITH IDS STRINGS LIKE : \"[\"abcde-1234-abcde-1234\", \"zyzdr-1234-zyzdr-1234\" ]\". The array should be formed in a JSON FORMAT,' +
+              'ONLY RETURN THE ARRAY WITH IDS STRINGS LIKE : "["abcde-1234-abcde-1234", "zyzdr-1234-zyzdr-1234" ]". The array should be formed in a JSON FORMAT,' +
               ' AND ONLY THE _id PROPERTY.' +
-              'IF NO DOCUMENTS IS SELECTED JUST RETURN AN EMPTY ARRAY LIKE THIS \"[]\". \n' +
-              'ONLY GET INFORMATION FROM THIS DOCUMENTS, DONT DO ANYTHING ELSE. HERE\'S THE DOCUMENTS: ' + text
+              'IF NO DOCUMENTS IS SELECTED JUST RETURN AN EMPTY ARRAY LIKE THIS "[]". \n' +
+              "ONLY GET INFORMATION FROM THIS DOCUMENTS, DONT DO ANYTHING ELSE. HERE'S THE DOCUMENTS: " +
+              text,
           },
           {
             role: 'user',
-            content: message
-          }
+            content: message,
+          },
         ],
         stream: false,
-      }
+      };
 
-      const t1 = performance.now()
+      const t1 = performance.now();
 
+      const reply: ChatCompletion | AsyncIterable<ChatCompletionChunk> =
+        await this.engine()!.chat.completions.create(
+          messagesToFindRelevantDocs
+        );
 
-      const reply: ChatCompletion | AsyncIterable<ChatCompletionChunk> = await this.engine()!.chat.completions.create(
-        messagesToFindRelevantDocs
-      );
-
-      const t2 = performance.now()
-
+      const t2 = performance.now();
 
       console.log('AI replied in...', t2 - t1);
 
-
-      if ('choices' in reply && reply.choices.length > 0 && reply.choices[0].message.content) {
-        response += reply.choices[0].message.content!
+      if (
+        'choices' in reply &&
+        reply.choices.length > 0 &&
+        reply.choices[0].message.content
+      ) {
+        response += reply.choices[0].message.content!;
       }
-
     }
 
-    return response
-
-
+    return response;
   }
 
-
-  private async answerQuestion(message: string, documents: string []) {
+  private async answerQuestion(message: string, documents: string[]) {
     if (!this.engine()) {
-      this.messages.update((prev) => [...prev, {
-        role: 'user',
-        content: `Engine not loaded, there was probably an error`
-      }]);
-      return ""
-
+      this.messages.update((prev) => [
+        ...prev,
+        {
+          role: 'user',
+          content: `Engine not loaded, there was probably an error`,
+        },
+      ]);
+      return '';
     }
 
-    const relevantDocs = JSON.stringify(this.documents().filter((doc) => {
-      return documents.find((documentId) => documentId === doc._id)
-    }).map((doc) => ({content: doc.content, title: doc.title})))
+    const relevantDocs = JSON.stringify(
+      this.documents()
+        .filter((doc) => {
+          return documents.find((documentId) => documentId === doc._id);
+        })
+        .map((doc) => ({ content: doc.content, title: doc.title }))
+    );
 
+    const texts = await this.engineService.splitText(relevantDocs, 500, 50);
+    let response = '';
 
-    const texts = await this.engineService.splitText(relevantDocs, 500, 50)
-    let response = ""
-
-    let totElapsed = 0
-
+    let totElapsed = 0;
 
     for (let text of texts) {
-
-      console.log(text)
+      console.log(text);
 
       const messages: ChatCompletionRequestBase = {
         messages: [
           {
             role: 'system',
-            content: this.content + '. Answer questions about documents contents, IF THERE IS NO RELEVANT INFORMATION IN THE DOCUMENT JUST RETURN "null" NOTHING ELSE.' +
-              ' this is the doc content split in different chunks: ' + text,
+            content:
+              this.content +
+              '. Answer questions about documents contents Please only use the information provided in the documents. You can deduce or infer things based on that information, but do not add any new details or content that isn\'t explicitly or implicitly present in the documents. Please do not generate or create any new names or characters, and only answer based on that.. IF THERE IS NO RELEVANT INFORMATION IN THE DOCUMENT JUST RETURN "null" NOTHING ELSE.' +
+              ' this is the doc content split in different chunks: ' +
+              text,
           },
           {
             role: 'user',
-            content: message
-          }
+            content: message,
+          },
         ],
         stream: false,
-        max_tokens: 600
-      }
+        max_tokens: 600,
+      };
 
-      const t1 = performance.now()
+      const t1 = performance.now();
 
-      const reply: ChatCompletion | AsyncIterable<ChatCompletionChunk> = await this.engine()!.chat.completions.create(
-        messages
-      );
+      const reply: ChatCompletion | AsyncIterable<ChatCompletionChunk> =
+        await this.engine()!.chat.completions.create(messages);
 
-      const t2 = performance.now()
-
+      const t2 = performance.now();
 
       console.log('AI replied in...', t2 - t1);
 
-      if ('choices' in reply && reply.choices.length > 0 && reply.choices[0].message.content) {
-        response += reply.choices[0].message.content!.toLowerCase().includes("null") ? "" : reply.choices[0].message.content!
+      if (
+        'choices' in reply &&
+        reply.choices.length > 0 &&
+        reply.choices[0].message.content
+      ) {
+        response += reply.choices[0].message
+          .content!.toLowerCase()
+          .includes('null')
+          ? ''
+          : reply.choices[0].message.content!;
       }
 
-      response += ""
+      response += '';
 
       totElapsed += t2 - t1;
 
-
       if (response.length >= 400) {
-
         //remove all " null " occurrences case insensitive
-        response = response.replace(/null/gi, "")
+        response = response.replace(/null/gi, '');
         console.log('>>> AI replied in TOTAL...', totElapsed, response);
 
-        return response
+        return response;
       }
-
     }
     console.log('>>> AI replied in TOTAL...', totElapsed);
 
-    return response === "" ? "I could not find an answer to this question in the ancient tomes" : response
-
+    return response === ''
+      ? 'I could not find an answer to this question in the ancient tomes'
+      : response;
   }
-
 }
