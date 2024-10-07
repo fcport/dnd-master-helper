3b0bc8f958f96f94854eda76bad7b277d060b75b
Federico Casadei <federico.casadei.portfolio@gmail.com>
2024-10-07 16:20:38 +0200

    fixL keywords

--- src/app/components/document-side-panel/document-side-panel.component.ts
+++ src/app/components/document-side-panel/document-side-panel.component.ts
@@ -1,51 +1,66 @@
-import {Component, inject, model, output, Signal, signal} from '@angular/core';
-import {injectAppSelector} from "../../injectables";
-import {selectActiveDocument} from "../../store/document-slice";
-import {Doc} from "../../models/db-response.model";
-import {CommonModule} from "@angular/common";
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {toSignal} from "@angular/core/rxjs-interop";
+import {
+  Component,
+  inject,
+  model,
+  output,
+  Signal,
+  signal,
+} from '@angular/core';
+import { injectAppSelector } from '../../injectables';
+import { selectActiveDocument } from '../../store/document-slice';
+import { Doc } from '../../models/db-response.model';
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-document-side-panel',
   standalone: true,
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './document-side-panel.component.html',
-  styleUrl: './document-side-panel.component.scss'
+  styleUrl: './document-side-panel.component.scss',
 })
 export class DocumentSidePanelComponent {
+  activeDocument = injectAppSelector(selectActiveDocument);
 
-  activeDocument = injectAppSelector(selectActiveDocument)
-
-  documentEdited = model({...this.activeDocument()})
-
+  documentEdited = model({ ...this.activeDocument() });
 
   fb = inject(FormBuilder);
-  form: FormGroup
+  form: FormGroup;
 
-  onSave = output<Partial<Doc>>()
-  onCancel = output<void>()
+  onSave = output<Partial<Doc>>();
+  onCancel = output<void>();
 
   constructor() {
     this.form = this.fb.group({
       content: this.fb.control<string>(this.documentEdited().content || ''),
-      keywords: this.fb.control<string>(this.documentEdited().keywords?.join() || ''),
+      keywords: this.fb.control<string>(
+        this.documentEdited().keywords?.join() || ''
+      ),
       summary: this.fb.control<string>(this.documentEdited().summary || ''),
       title: this.fb.control<string>(this.documentEdited().title || ''),
-
-    })
-
-
+    });
   }
 
-
   saveDocument() {
-    console.log('saveDocument', {...this.activeDocument(), ...this.form.value}, this.activeDocument())
-    this.onSave.emit({...this.activeDocument(), ...this.form.value})
+    console.log(
+      'saveDocument',
+      { ...this.activeDocument(), ...this.form.value },
+      this.activeDocument()
+    );
+    this.onSave.emit({
+      ...this.activeDocument(),
+      ...this.form.value,
+      keywords: this.form.value.keywords.split(','),
+    });
   }
 
   cancel() {
-    this.onCancel.emit()
+    this.onCancel.emit();
   }
-
 }
